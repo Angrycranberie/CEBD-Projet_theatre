9eb769686fe905b0c637ab46daf3d3d518f59832
Alexis YVON <alexis.yvon@gmail.com>
2019-11-28 15:13:45 +0100

    TODO 1.7 # Tickets displaying

--- actions/action_fct_comp_3.py
+++ actions/action_fct_comp_3.py
@@ -1,4 +1,3 @@
-
 import sqlite3
 from utils import display
 from PyQt5.QtWidgets import QDialog
@@ -12,16 +11,11 @@ from utils.display import refreshGenericCombo
 class AppFctComp3(QDialog):
 
     # Constructeur
-    def __init__(self, data:sqlite3.Connection):
+    def __init__(self, data: sqlite3.Connection):
         super(QDialog, self).__init__()
         self.ui = uic.loadUi("gui/fct_comp_3.ui", self)
         self.data = data
-        try:
-            cursor = self.data.cursor()
-            result = cursor.execute("SELECT catZone FROM LesZones")
-            refreshGenericCombo(self.ui.comboBox_fct_comp_3, result)
-        except Exception as e:
-            display.refreshLabel(self.ui.label_fct_comp_3, "Aucune catégorie trouvée")
+        self.refreshCatList()
 
     # Fonction de mise à jour de l'affichage
     @pyqtSlot()
@@ -39,3 +33,15 @@ class AppFctComp3(QDialog):
             i = display.refreshGenericData(self.ui.table_fct_comp_3, result)
             if i == 0:
                 display.refreshLabel(self.ui.label_fct_comp_3, "Aucun résultat")
+
+    # Fonction de mise à jour des catégories
+    @pyqtSlot()
+    def refreshCatList(self):
+        try:
+            cursor = self.data.cursor()
+            result = cursor.execute("SELECT catZone FROM LesZones")
+        except Exception as e:
+            self.ui.comboBox_fct_comp_3.clear()
+            display.refreshLabel(self.ui.label_fct_comp_3, "Aucune catégorie trouvée")
+        else:
+            display.refreshGenericCombo(self.ui.comboBox_fct_comp_3, result)
