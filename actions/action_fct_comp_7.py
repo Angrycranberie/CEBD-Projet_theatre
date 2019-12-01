7755323e026b0384d92b23b703f0c5982c37c9c9
Angrycranberie <mathdep38@sfr.fr>
2019-12-01 12:12:00 +0100

    TODO 3.1 ComboBox bug

--- actions/action_fct_comp_7.py
+++ actions/action_fct_comp_7.py
@@ -3,6 +3,7 @@ from utils import display
 from PyQt5.QtWidgets import QDialog
 from PyQt5.QtCore import pyqtSlot
 from PyQt5 import uic
+from utils.display import refreshGenericCombo
 
 
 # Classe permettant d'afficher la fonction à compléter 7
@@ -14,6 +15,7 @@ class AppFctComp7(QDialog):
         self.ui = uic.loadUi("gui/fct_comp_7.ui", self)
         self.data = data
         self.refreshResult()
+        self.refreshSpecList()
 
     # Fonction de mise à jour de l'affichage
     @pyqtSlot()
@@ -29,4 +31,30 @@ class AppFctComp7(QDialog):
         else:
             display.refreshGenericData(self.ui.table_fct_comp_7, result)
 
+    @pyqtSlot()
+    def refreshSpecList(self):
+        print("test")
+        try:
+            print("try passed")
+            cursor = self.data.cursor()
+            print("cursor")
+            result = cursor.execute("SELECT noSpec FROM LesSpectacles")
+            print("result")
+        except Exception as e:
+            self.ui.comboBox_ajout.clear()
+            display.refreshLabel(self.ui.label_fct_comp_7, "Aucune catégorie trouvée")
+        else:
+            print("excep")
+            display.refreshGenericCombo(self.ui.comboBox_ajout, result)
+
+
+
     def ajouter(self):
+        display.refreshLabel(self.ui.label_fct_comp_7, "")
+        spec = str(self.ui.comboBox_ajout.currentText())
+        try:
+            # cursor = self.data.cursor()
+            # result = cursor.execute("INSERT INTO LesRepresentations (noSpec, dateRep, promoRep)" VALUES ())
+            print(spec)
+        except Exception as e:
+            display.refreshLabel(self.ui.label_fct_comp_7, "Impossible d'afficher les résultats : " + repr(e))
