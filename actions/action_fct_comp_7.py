fb2a8cb226c64ea84349a3ee94243a69b77a99e6
Angrycranberie <mathdep38@sfr.fr>
2019-12-01 22:28:56 +0100

    TODO 3.1 # up

--- actions/action_fct_comp_7.py
+++ actions/action_fct_comp_7.py
@@ -5,7 +5,6 @@ from PyQt5.QtCore import pyqtSlot
 from PyQt5 import uic
 import time
 from datetime import datetime
-from utils.display import refreshGenericCombo
 
 
 # Classe permettant d'afficher la fonction à compléter 7
@@ -35,6 +34,7 @@ class AppFctComp7(QDialog):
         else:
             display.refreshGenericData(self.ui.table_fct_comp_7, result)
 
+    # mise à jour de la combo box des ajouts
     def refreshSpecList(self):
         try:
             cursor = self.data.cursor()
@@ -47,59 +47,85 @@ class AppFctComp7(QDialog):
             display.refreshGenericCombo(self.ui.comboBox_ajout_fct_comp_7, result)
             # display.refreshGenericCombo(self.ui.comboBox_suppr_fct_comp_7, result2)
 
+    # mise à jour de la combo box des suppressions pour les noSpec
     def refreshSpec_suppr_List(self):
-            try:
-                cursor = self.data.cursor()
-                result = cursor.execute("SELECT nomSpec FROM LesSpectacles")
-            except Exception as e:
-                self.ui.comboBox_suppr_fct_comp_7.clear()
-            else:
-                display.refreshGenericCombo(self.ui.comboBox_suppr_fct_comp_7, result)
+        try:
+            cursor = self.data.cursor()
+            result = cursor.execute("SELECT nomSpec FROM LesSpectacles")
+        except Exception as e:
+            self.ui.comboBox_suppr_fct_comp_7.clear()
+        else:
+            display.refreshGenericCombo(self.ui.comboBox_suppr_fct_comp_7, result)
 
+    # mise à jour de la combo box des suppressions pour les dateRep
     def refreshRepr_suppr_List(self):
-            try:
-                cursor = self.data.cursor()
-                result = cursor.execute("SELECT dateRep FROM LesRepresentations NATURAL JOIN LesSpectacles WHERE nomSpec = ? ",
-                                        [str(self.ui.comboBox_suppr_fct_comp_7.currentText())])
-            except Exception as e:
-                self.ui.comboBox_suppr_fct_comp_7_2.clear()
-            else:
-                display.refreshGenericCombo(self.ui.comboBox_suppr_fct_comp_7_2, result)
+        try:
+            cursor = self.data.cursor()
+            # requête avec paramètre '?'
+            result = cursor.execute(
+                "SELECT dateRep FROM LesRepresentations NATURAL JOIN LesSpectacles WHERE nomSpec = ? ",
+                [str(self.ui.comboBox_suppr_fct_comp_7.currentText())])
+        except Exception as e:
+            self.ui.comboBox_suppr_fct_comp_7_2.clear()
+        else:
+            display.refreshGenericCombo(self.ui.comboBox_suppr_fct_comp_7_2, result)
 
-
+    # ajout d'une représentation dans la table
     def ajouter(self):
         display.refreshLabel(self.ui.label_fct_comp_7, "")
         try:
             cursor = self.data.cursor()
+            # conditions pour que l'ajout ce fasse et que on ne peut pas vérifier en SQL
             if self.ui.comboBox_ajout_fct_comp_7.currentText() == "":
                 raise Exception("no Spec")
-            result = cursor.execute("SELECT dateRep FROM LesRepresentations WHERE dateRep = ?", [str(self.ui.dateTimeEdit.date())])
+            result = cursor.execute("SELECT dateRep FROM LesRepresentations WHERE dateRep = ?",
+                                    [str(self.ui.dateTimeEdit.date())])
             print(list(result))
             if len(result.fetchall()) != 0:
                 raise Exception("Representation existante ou salle prise")
             if self.ui.doubleSpinBox.value() > 1 or self.ui.doubleSpinBox.value() <= 0:
                 raise Exception("Promo doit être compris entre 0 et 1")
 
-            noSpecReq = cursor.execute("SELECT noSpec FROM LesSpectacles WHERE nomSpec = ?", [str(self.ui.comboBox_ajout_fct_comp_7.currentText())])
+            # récupération du noSpec
+            noSpecReq = cursor.execute("SELECT noSpec FROM LesSpectacles WHERE nomSpec = ?",
+                                       [str(self.ui.comboBox_ajout_fct_comp_7.currentText())])
             noSpecRepr = list(noSpecReq)[0][0]
+
+            # récupération de la dateRep
             dateRepr = self.ui.dateTimeEdit.dateTime().toPyDateTime()
             dateRepr = dateRepr.strftime("%d/%m/%Y %H:%M")
-            result = cursor.execute("INSERT INTO LesRepresentations_base (noSpec, dateRep, promoRep) VALUES(?, ?, ?)", [int(noSpecRepr), dateRepr, self.ui.doubleSpinBox.value()])
-            # print(dateRepr)
+
+            result = cursor.execute("INSERT INTO LesRepresentations_base (noSpec, dateRep, promoRep) VALUES(?, ?, ?)",
+                                    [int(noSpecRepr), dateRepr, self.ui.doubleSpinBox.value()])
+
+            # mise à jour persistante de la base de donnée
             result = cursor.execute("COMMIT")
+
         except Exception as e:
             display.refreshLabel(self.ui.label_fct_comp_7, "Impossible d'afficher les résultats : " + repr(e))
 
+    # suppression d'une représentation
     def suppr(self):
         display.refreshLabel(self.ui.label_fct_comp_7, "")
         try:
             cursor = self.data.cursor()
             noSpecReq = cursor.execute("SELECT noSpec FROM LesSpectacles WHERE nomSpec = ?",
                                        [str(self.ui.comboBox_ajout_fct_comp_7.currentText())])
+
+            # récupération du noSpec
             noSpec = list(noSpecReq)[0][0]
+
+            # récupérationn de la dateRep
             dateRepr = self.ui.comboBox_suppr_fct_comp_7_2.currentText()
-            # dateRepr = dateRepr.strftime("%d/%m/%Y %H:%M")
-            result = cursor.execute("DELETE FROM LesRepresentations_base WHERE noSpec = ? AND dateRep = ?", [noSpec,dateRepr ])
+
+            #suppréssion de la représentation
+            result = cursor.execute("DELETE FROM LesRepresentations_base WHERE noSpec = ? AND dateRep = ?",
+                                    [noSpec, dateRepr])
+            # suppression des tickets de cette représentaions
+            result = cursor.execute("DELETE FROM LesTickets WHERE noSpec = ? AND dateRep = ?", [noSpec, dateRepr])
+
+            # mise à jour persistante de la base de donnée
+            result = cursor.execute("COMMIT")
 
         except Exception as e:
             display.refreshLabel(self.ui.label_fct_comp_7, "Impossible d'afficher les résultats : " + repr(e))
