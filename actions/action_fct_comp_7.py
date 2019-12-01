28b5c96ad6770b59f50f4b1adec9a28c842fb7bb
Angrycranberie <mathdep38@sfr.fr>
2019-12-01 17:40:46 +0100

    TODO 3.1 # up
    Reste : fix insert and delete

--- actions/action_fct_comp_7.py
+++ actions/action_fct_comp_7.py
@@ -16,6 +16,8 @@ class AppFctComp7(QDialog):
         self.data = data
         self.refreshResult()
         self.refreshSpecList()
+        self.refreshSpec_suppr_List()
+        self.refreshRepr_suppr_List()
 
     # Fonction de mise à jour de l'affichage
     @pyqtSlot()
@@ -34,22 +36,54 @@ class AppFctComp7(QDialog):
     def refreshSpecList(self):
         try:
             cursor = self.data.cursor()
-            result = cursor.execute("SELECT noSpec FROM LesSpectacles")
-            print(list(result))
+            result = cursor.execute("SELECT nomSpec FROM LesSpectacles")
+            # result2 = result
         except Exception as e:
-            self.ui.comboBox_ajout.clear()
-            display.refreshLabel(self.ui.label_fct_comp_7, "Aucun Spectacle trouvée")
+            self.ui.comboBox_ajout_fct_comp_7.clear()
+            # self.ui.comboBox_suppr_fct_comp_7.clear()
         else:
-            display.refreshGenericCombo(self.ui.comboBox_ajout, result)
+            display.refreshGenericCombo(self.ui.comboBox_ajout_fct_comp_7, result)
+            # display.refreshGenericCombo(self.ui.comboBox_suppr_fct_comp_7, result2)
 
+    def refreshSpec_suppr_List(self):
+            try:
+                cursor = self.data.cursor()
+                result = cursor.execute("SELECT nomSpec FROM LesSpectacles")
+            except Exception as e:
+                self.ui.comboBox_suppr_fct_comp_7.clear()
+            else:
+                display.refreshGenericCombo(self.ui.comboBox_suppr_fct_comp_7, result)
+
+    def refreshRepr_suppr_List(self):
+            try:
+                cursor = self.data.cursor()
+                result = cursor.execute("SELECT dateRep FROM LesRepresentations NATURAL JOIN LesSpectacles WHERE nomSpec = ? ",
+                                        [str(self.ui.comboBox_suppr_fct_comp_7.currentText())])
+            except Exception as e:
+                self.ui.comboBox_suppr_fct_comp_7_2.clear()
+            else:
+                display.refreshGenericCombo(self.ui.comboBox_suppr_fct_comp_7_2, result)
 
 
     def ajouter(self):
         display.refreshLabel(self.ui.label_fct_comp_7, "")
-        spec = str(self.ui.comboBox_ajout.currentText())
+        spec = str(self.ui.comboBox_ajout_fct_comp_7.currentText())
         try:
-            # cursor = self.data.cursor()
-            # result = cursor.execute("INSERT INTO LesRepresentations (noSpec, dateRep, promoRep)" VALUES ())
+            cursor = self.data.cursor()
             print(spec)
+            if self.ui.comboBox_ajout_fct_comp_7.currentText() == "":
+                raise Exception("no Spec")
+            result = cursor.execute("SELECT dateRep FROM LesRepresentations WHERE dateRep = ?", [str(self.ui.dateTimeEdit.date())])
+            print(list(result))
+            if len(result.fetchall()) != 0:
+                raise Exception("Representation existante ou salle prise")
+            if self.ui.doubleSpinBox.value() > 1 or self.ui.doubleSpinBox.value() <= 0:
+                raise Exception("Promo doit être compris entre 0 et 1")
+
+            noSpecReq = cursor.execute("SELECT noSpec FROM LesSpectacles WHERE nomSpec = ?", [str(self.ui.comboBox_ajout_fct_comp_7.currentText())])
+            noSpecRepr = list(noSpecReq)[0][0]
+            dateRepr = self.ui.dateTimeEdit.date()
+            result = cursor.execute("INSERT INTO LesRepresentations_base (noSpec, dateRep, promoRep) VALUES(?, ?, ?)", [noSpecRepr, self.ui.dateTimeEdit.date(), self.ui.doubleSpinBox.value()])
+
         except Exception as e:
             display.refreshLabel(self.ui.label_fct_comp_7, "Impossible d'afficher les résultats : " + repr(e))
